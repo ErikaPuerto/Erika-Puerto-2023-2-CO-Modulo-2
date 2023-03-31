a8828512f1445a8f5756930e1d0359749368cc38
ErikaPuerto <erikapuerto1306@gmail.com>
2023-03-31 16:45:36 -0500

    First-Class

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -1,6 +1,7 @@
 import pygame
 
 from dino_runner.utils.constants import BG, ICON, SCREEN_HEIGHT, SCREEN_WIDTH, TITLE, FPS
+from dino_runner.components.dinosaur import Dinosaur
 
 
 class Game:
@@ -14,6 +15,9 @@ class Game:
         self.game_speed = 20
         self.x_pos_bg = 0
         self.y_pos_bg = 380
+        self.player = Dinosaur()
+        self.sound = pygame.mixer.music.load("xstep.ogg")
+        pygame.mixer.music.play(-1)
 
     def run(self):
         # Game loop: events - update - draw
@@ -22,6 +26,7 @@ class Game:
             self.events()
             self.update()
             self.draw()
+            self.sound
         pygame.quit()
 
     def events(self):
@@ -36,6 +41,7 @@ class Game:
         self.clock.tick(FPS)
         self.screen.fill((255, 255, 255))
         self.draw_background()
+        self.player.draw(self.screen)
         pygame.display.update()
         pygame.display.flip()
 
