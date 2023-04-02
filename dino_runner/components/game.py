20ffdec2c72ca092b4b771495786226cfc0622d4
ErikaPuerto <erikapuerto1306@gmail.com>
2023-04-01 22:07:19 -0500

    Second-Class-Homework

--- dino_runner/components/game.py
+++ dino_runner/components/game.py
@@ -29,6 +29,8 @@ class Game:
             self.sound
         pygame.quit()
 
+
+    
     def events(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -54,3 +56,5 @@ class Game:
             self.screen.blit(BG, (image_width + self.x_pos_bg, self.y_pos_bg))
             self.x_pos_bg = 0
         self.x_pos_bg -= self.game_speed
+
+
